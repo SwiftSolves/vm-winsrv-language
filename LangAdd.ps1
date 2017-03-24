0c0c503e6df36144d435cc7a06a859aa246f5299
Nathan Swift <SwiftSolves@users.noreply.github.com>
2017-03-23 22:42:21 -0400

    Add files via upload

---
+++ LangAdd.ps1
@@ -0,0 +1,35 @@
+﻿<# Custom Script for Windows to install DSIM language #>
+
+param (
+
+
+    [string]$winservertype,
+
+    [string]$lang
+
+
+)
+
+
+
+New-Item -ItemType Directory c:\temp
+
+#Download needed files from storage blob
+
+Invoke-WebRequest https://ctxlabs.blob.core.windows.net/installs/adksetup.exe -OutFile "c:\temp\adksetup.exe"
+
+$url = "https://ctxlabs.blob.core.windows.net/lps/$winservertype/$lang/lp.cab"
+
+Invoke-WebRequest $url -OutFile "c:\temp\lp.cab"
+
+#Install adk onto VM, this can take some time
+
+Start-Process c:\temp\adksetup.exe -ArgumentList '/installpath "C:\temp\MyTools\mykit" /features + /q' -Wait
+
+#Inject language file into VM
+
+Add-WindowsPackage –Online –PackagePath C:\temp\lp.cab
+
+#Restart VM for language file to be avaliable
+
+Restart-Computer -ComputerName "localhost"
